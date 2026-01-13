65b9f6686f732fb27b6c3a2652c30bf3543ff785
iamTinMan67 <tomboyce01@gmail.com>
2026-01-13 14:08:34 +0000

    Update vite config for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react()],
   server: {
     port: 8080,
     open: true
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    target: 'esnext',
+    minify: 'esbuild'
   }
 })
