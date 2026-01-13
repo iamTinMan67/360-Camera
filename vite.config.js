13c348886314a5b9dc99deefc0bab81379e4d822
iamTinMan67 <tomboyce01@gmail.com>
2026-01-13 14:01:47 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [react()],
   server: {
     port: 8080,
     open: true
   },
   build: {
-    rollupOptions: {
-      input: './index.html'
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
